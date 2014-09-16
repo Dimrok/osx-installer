6db9d1b9951c12873c51463824d106f5dd64227e
Christopher Crone <christopher.crone@infinit.io>
2014-09-16 19:03:58 +0200

    Use ARC and Infinit code norm.

--- InfinitInstallFinisher/InfinitInstallFinisher.m
+++ InfinitInstallFinisher/InfinitInstallFinisher.m
@@ -10,124 +10,107 @@
 
 #define INFINIT_APP_PATH @"/Applications/Infinit.app"
 
-@interface InfinitTerminationListener : NSObject {
-    
-	pid_t parentProcessID;
-    NSString *appPath;
+@interface InfinitTerminationListener : NSObject
+{
+@private
+  pid_t _parent_pid;
+  NSString* _app_path;
 }
 
-- (id)initWithParentProcessID:(pid_t)ppid installLocation:(NSString *)path;
+- (id)initWithParentProcessID:(pid_t)ppid installLocation:(NSString*)path;
 
 @end
 
 @implementation InfinitTerminationListener
 
-- (id)initWithParentProcessID:(pid_t)ppid installLocation:(NSString *)path {
+- (id)initWithParentProcessID:(pid_t)ppid installLocation:(NSString*)path
+{
+  if ((self = [super init]))
+  {
+    _parent_pid = ppid;
+    _app_path = [path copy];
 
-    if ((self = [super init])) {
-     
-        parentProcessID = ppid;
-        appPath = [path copy];
-        
-        BOOL alreadyTerminated = (getppid() == 1); // ppid is launchd (1) => parent terminated already
-        
-        if (alreadyTerminated) {
-            
-            [self finishInstallationAndLaunchInfinit];
-        }
-        else {
-            
-            NSNotificationCenter *notificationCenter;
-            notificationCenter = [[NSWorkspace sharedWorkspace] notificationCenter];
-            [notificationCenter addObserver:self
-                                   selector:@selector(applicationDidTerminate:)
-                                       name:NSWorkspaceDidTerminateApplicationNotification
-                                     object:nil];
-        }
+    BOOL already_terminated = (getppid() == 1); // ppid is launchd (1) => parent terminated already
+
+    if (already_terminated)
+    {
+      [self finishInstallationAndLaunchInfinit];
     }
-	return self;
+    else
+    {
+      [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
+                                                             selector:@selector(applicationDidTerminate:)
+                                                                 name:NSWorkspaceDidTerminateApplicationNotification
+                                                               object:nil];
+    }
+  }
+  return self;
 }
 
-- (void)applicationDidTerminate:(NSNotification *)notification {
-    
-    NSDictionary *info = [notification userInfo];
-    pid_t terminatedProcessID = [[info valueForKey:@"NSApplicationProcessIdentifier"] intValue];
+- (void)applicationDidTerminate:(NSNotification*)notification
+{
+  NSDictionary* info = notification.userInfo;
+  pid_t terminated_pid = [[info valueForKey:@"NSApplicationProcessIdentifier"] intValue];
 
-    if (parentProcessID == terminatedProcessID) {
-
-        [self finishInstallationAndLaunchInfinit];
-    }
+  if (_parent_pid == terminated_pid)
+    [self finishInstallationAndLaunchInfinit];
 }
 
 - (void)finishInstallationAndLaunchInfinit {
-    
-    NSLog(@"Parent process (%d) has quit.", parentProcessID);
-    NSLog(@"Finishing Installation of: %@", appPath);
-    
-    NSError *error = nil;
-    NSFileManager *mgr = [NSFileManager defaultManager];
-    
-    if ([mgr fileExistsAtPath:INFINIT_APP_PATH]) {
-        
-        [mgr removeItemAtPath:INFINIT_APP_PATH error:&error];
-    }
-    
-    [mgr copyItemAtPath:appPath toPath:INFINIT_APP_PATH error:&error];
-    
-    [[NSWorkspace sharedWorkspace] launchApplication:INFINIT_APP_PATH];
 
-    [NSApp terminate:nil];
+  NSLog(@"Parent process (%d) has quit.", _parent_pid);
+  NSLog(@"Finishing Installation of: %@", _app_path);
+
+  NSError* error = nil;
+  NSFileManager* mgr = [NSFileManager defaultManager];
+
+  if ([mgr fileExistsAtPath:INFINIT_APP_PATH])
+    [mgr removeItemAtPath:INFINIT_APP_PATH error:&error];
+
+  [mgr copyItemAtPath:_app_path toPath:INFINIT_APP_PATH error:&error];
+
+  [[NSWorkspace sharedWorkspace] launchApplication:INFINIT_APP_PATH];
+
+  [NSApp terminate:nil];
 }
 
-- (void)dealloc {
-	
-    NSNotificationCenter *notificationCenter;
-    notificationCenter = [[NSWorkspace sharedWorkspace] notificationCenter];
-    [notificationCenter removeObserver:self];
-    
-    [appPath release];
-    
-	[super dealloc];
+- (void)dealloc
+{
+  [[[NSWorkspace sharedWorkspace] notificationCenter] removeObserver:self];
 }
 
 @end
 
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
+  int status = EXIT_SUCCESS;
+  @autoreleasepool
+  {
+    if (argc == 3)
+    {
+      pid_t parent_pid = atoi(argv[1]);
+      NSString* app_path = [NSString stringWithUTF8String:argv[2]];
+      BOOL app_exists = [[NSFileManager defaultManager] fileExistsAtPath:app_path];
 
-    int status = EXIT_SUCCESS;
-    
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    
-    if (argc == 3) {
-
-        pid_t parentProcessID = atoi(argv[1]);
-        NSString *appPath = [NSString stringWithUTF8String:argv[2]];
-        BOOL appExistsAtPath = [[NSFileManager defaultManager] fileExistsAtPath:appPath];
-        
-        if (parentProcessID != 0 && appExistsAtPath) {
-            
-            InfinitTerminationListener *listener;
-            listener = [[[InfinitTerminationListener alloc]
-                         initWithParentProcessID:parentProcessID installLocation:appPath] autorelease];
- 
-            [[NSApplication sharedApplication] run];
-        }
-        else {
-         
-            NSLog(@"%s invalid arguments.", argv[0]);
-
-            status = EXIT_FAILURE;
-        }
-    }
-    else {
-
-        NSLog(@"%s invalid number of arguments.", argv[0]);
-
+      if (parent_pid != 0 && app_exists)
+      {
+        InfinitTerminationListener* listener;
+        listener = [[InfinitTerminationListener alloc] initWithParentProcessID:parent_pid
+                                                               installLocation:app_path];
+        [[NSApplication sharedApplication] run];
+      }
+      else
+      {
+        NSLog(@"%s invalid arguments.", argv[0]);
         status = EXIT_FAILURE;
+      }
     }
-    
-	[pool drain];
-	
-	return status;
+    else
+    {
+      NSLog(@"%s invalid number of arguments.", argv[0]);
+      status = EXIT_FAILURE;
+    }
+  }
+  return status;
 }
