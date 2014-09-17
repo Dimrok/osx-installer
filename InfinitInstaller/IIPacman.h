b0222299bf3282d5241512c64a7f1c221eba8c50
Christopher Crone <christopher.crone@infinit.io>
2014-09-17 13:26:59 +0200

    Add resources and animated pacman.

---
+++ InfinitInstaller/IIPacman.h
@@ -0,0 +1,15 @@
+//
+//  IIPacman.h
+//  InfinitInstaller
+//
+//  Created by Christopher Crone on 17/09/14.
+//  Copyright (c) 2014 Infinit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IIPacman : NSView
+
+@property (nonatomic, readwrite) BOOL animate;
+
+@end
